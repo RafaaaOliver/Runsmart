68eb20c1e951c848a001c5c0857ee260389c13c8
RafaaaOliver <you@example.com>
2022-05-04 12:43:28 -0300

    update

--- Trabalho/Código_apontamento/rsc_ue_apontamentos.js
+++ Trabalho/Código_apontamento/rsc_ue_apontamentos.js
@@ -6,76 +6,76 @@
 
 define(['N/search', 'N/record', 'N/log'],
 function(search, record, log){
-    function afterSubmit(ctx){
+    function beforeLoad(ctx){
+        try{
+            var lista = []
+            var page = ctx.newRecord;
+            if(ctx.type != ctx.UserEventType.EDIT){
+                var casoId = page.id
+                var cataHora = search.create({
+                    type: 'customrecord1452',
+                    filters:[
+                        'custrecord1498', 'IS', casoId
+                    ],
+                    columns:['custrecord_rsc_horasatuadas_rafael']
+                }).run().each(function(result) {
+                    lista.push(result.getValue('custrecord_rsc_horasatuadas_rafael'))
+                    return true
+                })
 
-        var lista = []
-        var page = ctx.newRecord;
-        var casoId = page.id
-        var cataHora = search.create({
-            type: 'customrecord1452',
-            filters:[
-                'custrecord1498', 'IS', casoId
-            ],
-            columns:['custrecord_rsc_horasatuadas_rafael']
-        }).run().each(function(result) {
-            lista.push(result.getValue('custrecord_rsc_horasatuadas_rafael'))
-            return true
-        })
+                var valorOriginal = page.getValue('custevent6')
+                log.debug('valor da lista' , lista)
+                log.debug('valor do hora casos', valorOriginal)
+                var listaNova = []
 
-        var valorOriginal = page.getValue('custevent6')
-        log.debug('valor da lista' , lista)
-        log.debug('valor do hora casos', valorOriginal)
-        var listaNova = []
-
-        for (i in lista){
-            var pontos = lista[i].indexOf(':')
-            var horas = lista[i].slice(0, pontos)
-            var minutos = lista[i].slice(pontos +1)
-            horas = Number(horas)
-            minutos = Number(minutos)
-            var HoraMin = horas*60 + minutos
-            listaNova.push(HoraMin)
+                for (i in lista){
+                    var pontos = lista[i].indexOf(':')
+                    var horas = lista[i].slice(0, pontos)
+                    var minutos = lista[i].slice(pontos +1)
+                    horas = Number(horas)
+                    minutos = Number(minutos)
+                    var HoraMin = horas*60 + minutos
+                    listaNova.push(HoraMin)
+                }
+                log.debug('id da pg caso', casoId)
+                log.debug('lista de valores', listaNova)
+                var soma = 0
+                for ( i in listaNova){
+                    soma += listaNova[i]
+                }        
+                var hora = Math.floor(soma/60)
+                var minuto = soma%60
+                
+                if(minuto >=10){
+                    minuto = minuto.toString()
+                }else{
+                    minuto = minuto.toString()
+                    minuto = '0' + minuto
+                }
+                
+                hora = hora.toString()
+                
+                var final = (hora + ':' + minuto)
+                
+                var load1 = record.load({
+                    type: 'supportcase',
+                    id: casoId
+                })
+                load1.setValue({
+                    fieldId:'custevent6',
+                    value: final,
+                    ignoreFieldChange: true})
+                load1.save({ignoreMandatoryFields: true})
+                // log.debug('hora', hora)
+                // log.debug('minuto', minuto)
+                // log.debug('valor final', final)
+                // log.debug('valor do campo', valorFinal)
+            }
+        }catch(e){
+            log.debug('Erro', e )
+            }
         }
-        log.debug('id da pg caso', casoId)
-        log.debug('lista de valores', listaNova)
-        var soma = 0
-        for ( i in listaNova){
-            soma += listaNova[i]
-        }        
-        var hora = Math.floor(soma/60)
-        var minuto = soma%60
-        if(minuto >=10){
-            minuto = minuto.toString()
-        }else{
-            minuto = minuto.toString()
-            minuto = '0' + minuto
-        }
-        
-        hora = hora.toString()
-        
-        var final = (hora + ':' + minuto)
-        
-        
-        var load1 = record.load({
-            type: 'supportcase',
-            id: casoId
-        })
-        load1.setValue({
-            fieldId:'custevent6',
-            value: final,
-            ignoreFieldChange: true})
-        load1.save({ignoreMandatoryFields: true})
-        
-        var valorFinal = page.getValue('custevent6')
-        // log.debug('hora', hora)
-        // log.debug('minuto', minuto)
-        // log.debug('valor final', final)
-        // log.debug('valor do campo', valorFinal)
-        
-        
-
-    }
     return{
-        afterSubmit: afterSubmit
+        beforeLoad: beforeLoad
     }
 })
