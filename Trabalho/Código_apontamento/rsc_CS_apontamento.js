fb23305fba682654f9fd6d14bde9b841ecc75bbf
RafaaaOliver <you@example.com>
2022-05-04 14:33:56 -0300

    Update

--- Trabalho/Código_apontamento/rsc_CS_apontamento.js
+++ Trabalho/Código_apontamento/rsc_CS_apontamento.js
@@ -4,50 +4,56 @@
 * @Authors Rafael Oliveira & Gabriel Pavia
 */
 define(['N/currentRecord','N/search', 'N/log', 'N/ui/dialog'], function(record, search, log, dialog){
+/**
+ * a função saveRecord precisa de um retorno true or false
+ * true (deixa salvar)
+ * false (não deixa salvar)
+ */
 
-    function saveRecord(ctx){
+    function saveRecord(ctx){ // função que será executada ao salvar
         try{
             var apontamento = ctx.currentRecord
-            console.log('Esperança')
+            console.log('Estou funcionando...')
             // if (ctx.fieldId == 'custrecord1500'){
-                var field = apontamento.getValue('custrecord_rsc_horasatuadas_rafael')
-                var pontos = field.indexOf(':')
-                var horas = field.slice(0, pontos)
-                var minutos = field.slice(pontos +1)
+                var field = apontamento.getValue('custrecord_rsc_horasatuadas_rafael')  // pega o valor do campo Horas atuadas
+                var pontos = field.indexOf(':') // pega o indice que contém os dois pontos
+                var horas = field.slice(0, pontos) // fatia do começo até os pontos e coloca em uma variável
+                var minutos = field.slice(pontos +1) // pega o conteúdo a partir do ponto e colcoa em variável
                 console.log('Estou rodando...')
                 console.log(horas)
                 console.log(minutos)
-
-                var ErroTamanho = {
+                // >>>>>>>>>>>>>>   Mensagens de erro <<<<<<<<<<<<<<<
+                var ErroTamanho = {   
                     title: 'Tamanho inválido!!!',
                     message: 'Por favor, forneça no máximo cinco caracteres (hh:mm)'
                 }
-
                 var ErroTipo = {
                     title: 'O formato está inválido!!!',
                     message: 'Digite as horas de acordo com o seguinte exemplo: (hh:mm)'
                 }
-                if(field){
-                    if(field.length == 5){
-                        if(horas == Number(horas) && minutos == Number(minutos) && field[2] == ':'){
-                            return true
-                        }else{
-                            dialog.alert(ErroTipo)
-                            return false
+                // >>>>>>>>>>>>>>>>> condicionais para verificação do campo <<<<<<<<<<<<<<<<<<<
+                if(field){  // se tiver algo nele, faça:
+                    if(field.length == 5){ // condição que limita o tamanho do campo para 5 caracteres
+                        // para o JavaScript  '1' == 1, então eu verifiquei se a string pode é numérica ou apenas texto
+                        if(horas == Number(horas) && minutos == Number(minutos) && field[2] == ':' && field[3] < 6){ // confere se são números e se tem o dois pontos
+                            return true // deixa salvar
+                        }else{ 
+                            dialog.alert(ErroTipo) // usa a mensagem de erro da linha 26
+                            return false // não deixa salvar
                         }
                     }else{
-                        dialog.alert(ErroTamanho)
-                        return false
+                        dialog.alert(ErroTamanho) // usa a mensagem de erro da linha 21
+                        return false // não deixa salvar 
                     }
-                }else{
-                    return true
+                }else{ // se o campo estiver vazio, deixa salvar 
+                    return true // deixa salvar
                 }
-        }catch(e){
-            log.debug('Erro', e)
+        }catch(e){ // recebe um erro 
+            log.debug('Erro', e)  // mostra o erro
         }
     }
     
     return{
-        saveRecord: saveRecord
+        saveRecord: saveRecord // usa a função
     }
 })
--- Trabalho/Código_apontamento/rsc_ue_apontamentos.js
+++ Trabalho/Código_apontamento/rsc_ue_apontamentos.js
@@ -6,73 +6,74 @@
 
 define(['N/search', 'N/record', 'N/log'],
 function(search, record, log){
-    function beforeLoad(ctx){
-        try{
+    function beforeLoad(ctx){ // função que é ativada antes de carregar a página 
+        try{ 
             var lista = []
-            var page = ctx.newRecord;
-            if(ctx.type != ctx.UserEventType.EDIT){
-                var casoId = page.id
-                var cataHora = search.create({
-                    type: 'customrecord1452',
+            var page = ctx.newRecord; // pega o contexto da página
+            if(ctx.type != ctx.UserEventType.EDIT){  // verifica se a página está no modo editar
+                var casoId = page.id 
+                var cataHora = search.create({  // cria uma busca pelos registros com as seguintes características:
+                    type: 'customrecord1452',  // tipo do registro
                     filters:[
-                        'custrecord1498', 'IS', casoId
+                        'custrecord1498', 'IS', casoId // se tem o caso(variável) específico dentro do campo caso do registro
                     ],
-                    columns:['custrecord_rsc_horasatuadas_rafael']
-                }).run().each(function(result) {
-                    lista.push(result.getValue('custrecord_rsc_horasatuadas_rafael'))
-                    return true
+                    columns:['custrecord_rsc_horasatuadas_rafael'] // traz o valor da coluna específica
+                }).run().each(function(result) { // o .run() roda a pesquisa e para cada resultado o .each faz  
+                                                                            // uma ação por meio de uma função
+                    lista.push(result.getValue('custrecord_rsc_horasatuadas_rafael')) // adiciona o valor do campo a lista
+                    return true // precisa retornar true 
                 })
 
-                var valorOriginal = page.getValue('custevent6')
+                var valorOriginal = page.getValue('custevent6') // campo hora casos do registro caso
                 log.debug('valor da lista' , lista)
                 log.debug('valor do hora casos', valorOriginal)
                 var listaNova = []
 
-                for (i in lista){
-                    var pontos = lista[i].indexOf(':')
-                    var horas = lista[i].slice(0, pontos)
-                    var minutos = lista[i].slice(pontos +1)
+                for (i in lista){ // lista criada com a pesquisa da linha 15
+                    var pontos = lista[i].indexOf(':') // pega o indice que contém os dois pontos
+                    var horas = lista[i].slice(0, pontos) // fatia do começo até os pontos e coloca em uma variável
+                    var minutos = lista[i].slice(pontos +1) // pega o conteúdo a partir do ponto e colcoa em variável
                     horas = Number(horas)
                     minutos = Number(minutos)
-                    var HoraMin = horas*60 + minutos
-                    listaNova.push(HoraMin)
+                    var HoraMin = horas*60 + minutos // transforma a hora em minuto e soma com os minutos
+                    listaNova.push(HoraMin)  // coloca a data formatada em minutos em uma lista para facilitar a soma
                 }
                 log.debug('id da pg caso', casoId)
                 log.debug('lista de valores', listaNova)
                 var soma = 0
                 for ( i in listaNova){
-                    soma += listaNova[i]
+                    soma += listaNova[i] // soma todos os minutos/horas da lista 
                 }        
-                var hora = Math.floor(soma/60)
-                var minuto = soma%60
+                var hora = Math.floor(soma/60) // divisão truncada para pegar as horas
+                var minuto = soma%60 // divisão com "mod" para pegar o resto (minutos)
                 
-                if(minuto >=10){
+                if(minuto >=10){ // faz a verificação para o minuto não ficar apenas com uma casa
                     minuto = minuto.toString()
                 }else{
                     minuto = minuto.toString()
                     minuto = '0' + minuto
                 }
                 
-                hora = hora.toString()
+                hora = hora.toString() // transforma o número em string novamente
                 
-                var final = (hora + ':' + minuto)
+                var final = (hora + ':' + minuto) // constroi o formato hora por meio de concatenação
                 
-                var load1 = record.load({
+                var load1 = record.load({ // carrega o registro
                     type: 'supportcase',
                     id: casoId
                 })
-                load1.setValue({
+                load1.setValue({ // muda o campo do registro
                     fieldId:'custevent6',
                     value: final,
                     ignoreFieldChange: true})
-                load1.save({ignoreMandatoryFields: true})
+                load1.save({ignoreMandatoryFields: true}) // e o salva novamente
                 // log.debug('hora', hora)
                 // log.debug('minuto', minuto)
                 // log.debug('valor final', final)
                 // log.debug('valor do campo', valorFinal)
             }
         }catch(e){
-            log.debug('Erro', e )
+            log.debug('Erro', e ) // recebe qualquer e retorna uma mensagem
             }
         }
     return{
