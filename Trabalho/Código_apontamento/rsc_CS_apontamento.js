805d3fc7203733eb8a9de66365ae2db792b29e6f
Rafael Oliveira <99557624+RafaaaOliver@users.noreply.github.com>
2022-06-07 10:30:07 -0300

    New commit

--- Trabalho/Código_apontamento/rsc_CS_apontamento.js
+++ Trabalho/Código_apontamento/rsc_CS_apontamento.js
@@ -3,7 +3,7 @@
 * @NApiVersion 2.0
 * @Authors Rafael Oliveira & Gabriel Pavia
 */
-define(['N/currentRecord','N/search', 'N/log', 'N/ui/dialog'], function(record, search, log, dialog){
+define(['N/log', 'N/ui/dialog'], function(log, dialog){
     /**
      * a função saveRecord precisa de um retorno true or false
      * true (deixa salvar)
@@ -11,25 +11,21 @@ define(['N/currentRecord','N/search', 'N/log', 'N/ui/dialog'], function(record, 
      */
         function saveRecord(ctx){ // função que será executada ao salvar
             try{
-                console.log('Estou funcionando...')
                 var apontamento = ctx.currentRecord
                 var horasTotaisTrabalhadas = apontamento.getValue('custrecord_rsc_horasatuadas_rafael')  // pega o valor do campo Horas atuadas
-                var horasFaturaveis = apontamento.getValue('custrecord_apontamento_alocadascampo')
+                var horasFaturaveis = apontamento.getValue('custrecord_apontamento_alocadascampo') 
                 var textFaturaveis = 'Horas faturáveis'
                 var textTotais = 'Horas totais trabalhadas'
 
                 var returnando1 = requisisoes(horasTotaisTrabalhadas, textTotais)
                 var returnando2 = requisisoes(horasFaturaveis, textFaturaveis)
                 var retorno; 
-                log.debug('returnando1: ', returnando1)
-                log.debug('returnando2: ', returnando2)
 
                 if (returnando1 == true && returnando2 == true){
                     retorno = true
                 }else{
                     retorno = false
                 }
-                log.debug('retorno', retorno)
                 return retorno
             }catch(e){ // recebe um erro 
                 log.debug('Erro', e)  // mostra o erro
@@ -38,13 +34,8 @@ define(['N/currentRecord','N/search', 'N/log', 'N/ui/dialog'], function(record, 
 
         function requisisoes(Horas, nome){
             var virgula = Horas.indexOf(',') 
-            log.debug('Print do campo inteiro: ', Horas)
-            log.debug('Índice da virgula: ', virgula)
             var horas = Horas.slice(0, virgula)
             var minutos = Horas.slice(virgula +1)
-            log.debug('Horas: ', horas)
-            log.debug('Minutos: ', minutos)
-            log.debug('campos: ', Horas[1], Horas[2], Horas[3])
             // >>>>>>>>>>>>>>   Mensagens de erro <<<<<<<<<<<<<<<
             var ErroLimite = {
                 title: 'Você está fora da quantidade padrão de caracteres!!!',
@@ -79,9 +70,8 @@ define(['N/currentRecord','N/search', 'N/log', 'N/ui/dialog'], function(record, 
                 return true // deixa salvar
             }
         }
-        
+    
         return{
             saveRecord: saveRecord
-            // requisisoes: requisisoes // usa a função
         }
     })
