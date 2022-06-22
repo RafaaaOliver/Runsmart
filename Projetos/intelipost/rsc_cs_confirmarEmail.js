9dc52305a77cf0568caf07e7ec5fc8c28a54f909
rafaellstos@hotmail.com <rafaellstos@hotmail.com>
2022-06-22 17:18:16 -0300

    Adicionando o UserEvent e modificando o CS

--- Projetos/intelipost/rsc_cs_confirmarEmail.js
+++ Projetos/intelipost/rsc_cs_confirmarEmail.js
@@ -2,28 +2,20 @@
  *@NApiVersion 2.1
  *@NScriptType ClientScript
  */
- define(['N/log','N/currentRecord'], function(log, currentRecord) {
+ define(['N/log','N/email'], function(log, email) {
 
     function pageInit(ctx) {
-        var currRecord = ctx.currentRecord
-        log.debug(title, 'Script executando...')
-        console.log('Script executando...')
-    }
 
-    function validateField(ctx) {
-        log.debug('validate', 'Entrou no validateField...')
-        var customer = ctx.currentRecord;
-        if (ctx.fieldId == 'custpageworkflow773'){
-            console.log('Achei o botão')
-            log.debug('title','Achei o botão')
-        }
     }
 
     
+    function emailDisparo(){
+        log.debug('teste', 'entrou na func do user')
+    }
+    
 
     return {
-        pageInit: pageInit,
-        validateField: validateField
+        pageInit: pageInit
     }
 });
     
--- Projetos/intelipost/rsc_ue_botaoCadastro.js
+++ Projetos/intelipost/rsc_ue_botaoCadastro.js
@@ -7,7 +7,12 @@ define([], function() {
     function beforeLoad(ctx) {
         var form = ctx.form         
         if(ctx.type == ctx.UserEventType.EDIT || ctx.type == ctx.UserEventType.VIEW){
-
+            form.addButton({
+                id:'custpage_cadastroCliente',
+                label:'Enviar solicitação cadastro',
+                functionName:'emailDisparo'
+            })
+            form.clientScriptModulePath = ("./rsc_cs_confirmarEmail.js");
         }
     }
 
